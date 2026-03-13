8ee49b348906bd7d4297c4c12fb448f81f4a5317
sebiecula156-hue <sebi.ecula156@gmail.com>
2026-03-13 16:04:41 +0200

    first commit

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/autoclicker/chestii/test
+# Install script for directory: D:/autoclicker/chestii/blackjack
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/autoclicker/chestii/test/build/install_local_manifest.txt"
+  file(WRITE "D:/autoclicker/chestii/blackjack/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/autoclicker/chestii/test/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/autoclicker/chestii/blackjack/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
